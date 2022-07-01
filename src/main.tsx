05f25b94112fda1b7f233a1de73e14c18b52fbf4
Jess <toxocious@gmail.com>
2022-06-30 23:49:12 -0700

    Add and use MantineProvider for the app

--- src/main.tsx
+++ src/main.tsx
@@ -1,14 +1,15 @@
-import React from 'react';
 import ReactDOM from 'react-dom/client';
 import { BrowserRouter } from 'react-router-dom';
+
+import { MantineProvider } from '@mantine/core';
 
 import App from './App';
 import './main.css';
 
 ReactDOM.createRoot(document.getElementById('root')!).render(
-  <React.StrictMode>
+  <MantineProvider theme={{ colorScheme: 'dark' }}>
     <BrowserRouter>
       <App />
     </BrowserRouter>
-  </React.StrictMode>
+  </MantineProvider>
 );
