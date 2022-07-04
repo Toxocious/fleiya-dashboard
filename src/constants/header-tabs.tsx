fe41c122409094cb8d89ab5d42925c405657a4de
Jess <toxocious@gmail.com>
2022-07-03 17:13:16 -0700

    Refactor header-tabs as array of objects

--- src/constants/header-tabs.tsx
+++ src/constants/header-tabs.tsx
@@ -1,1 +1,32 @@
-export const HeaderTabsAllowed = ['Home', 'News', 'Pokedex', 'Map Explorer'];
+export const HeaderTabsAllowed = [
+  {
+    id: 0,
+    label: 'Home',
+    hiddenToGuests: false,
+  },
+  {
+    id: 1,
+    label: 'News',
+    hiddenToGuests: false,
+  },
+  {
+    id: 2,
+    label: 'Pokedex',
+    hiddenToGuests: false,
+  },
+  {
+    id: 3,
+    label: 'Itemdex',
+    hiddenToGuests: false,
+  },
+  {
+    id: 4,
+    label: 'Guides',
+    hiddenToGuests: false,
+  },
+  {
+    id: 5,
+    label: 'Map Explorer (Beta)',
+    hiddenToGuests: true,
+  },
+];
