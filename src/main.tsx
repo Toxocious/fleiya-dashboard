00af1bf448bf4617e985527dfd7bb4309f0bd3da
Jess <toxocious@gmail.com>
2022-07-05 21:34:34 -0700

    Set dark color scheme colors

--- src/main.tsx
+++ src/main.tsx
@@ -6,7 +6,27 @@ import App from './App';
 import './main.css';
 
 ReactDOM.createRoot(document.getElementById('root')!).render(
-  <MantineProvider theme={{ colorScheme: 'dark' }}>
+  <MantineProvider
+    theme={{
+      colorScheme: 'dark',
+      defaultRadius: 6,
+      loader: 'oval',
+      colors: {
+        dark: [
+          '#20c997',
+          '#36cea1',
+          '#4cd3ab',
+          '#62d9b6',
+          '#79dec0',
+          '#2C2E33',
+          '#25262B',
+          '#1A1B1E',
+          '#141517',
+          '#101113',
+        ],
+      },
+    }}
+  >
     <App />
   </MantineProvider>
 );
