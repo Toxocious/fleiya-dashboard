4ce064561164fd6deeeb59ab80c2781d48b4212d
Jess <toxocious@gmail.com>
2022-07-07 23:38:36 -0700

    Add array containing all features (placeholder)

---
+++ src/constants/feature_list.tsx
@@ -0,0 +1,38 @@
+export const FeatureList = [
+  {
+    id: 0,
+    name: 'Feature Name',
+    premium: false,
+    description:
+      'Lorem Ipsum is simply dummy text of the printing and typesetting industry.',
+    image:
+      'https://images.unsplash.com/photo-1527004013197-933c4bb611b3?ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&ixlib=rb-1.2.1&auto=format&fit=crop&w=720&q=80',
+  },
+  {
+    id: 2,
+    name: 'Feature Name',
+    premium: false,
+    description:
+      "Lorem Ipsum is simply dummy text of the printing and typesetting industry. Lorem Ipsum has been the industry's standard dummy text ever since the 1500s.",
+    image:
+      'https://images.unsplash.com/photo-1527004013197-933c4bb611b3?ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&ixlib=rb-1.2.1&auto=format&fit=crop&w=720&q=80',
+  },
+  {
+    id: 2,
+    name: 'Feature Name',
+    premium: false,
+    description:
+      "Lorem Ipsum is simply dummy text of the printing and typesetting industry. Lorem Ipsum has been the industry's standard dummy text ever since the 1500s.",
+    image:
+      'https://images.unsplash.com/photo-1527004013197-933c4bb611b3?ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&ixlib=rb-1.2.1&auto=format&fit=crop&w=720&q=80',
+  },
+  {
+    id: 3,
+    name: 'Feature Name',
+    premium: true,
+    description:
+      "Lorem Ipsum is simply dummy text of the printing and typesetting industry. Lorem Ipsum has been the industry's standard dummy text ever since the 1500s.",
+    image:
+      'https://images.unsplash.com/photo-1527004013197-933c4bb611b3?ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&ixlib=rb-1.2.1&auto=format&fit=crop&w=720&q=80',
+  },
+];
