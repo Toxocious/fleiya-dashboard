4302bbd9d27c1a892453ad8fb77f4e4197993fd8
Jess <toxocious@gmail.com>
2022-07-11 16:25:46 -0700

    Add 'pokedex_entries' constant templating

---
+++ src/constants/pokedex_entries.tsx
@@ -0,0 +1,15 @@
+interface iPokedexEntry {
+  id: number | string;
+  name: string;
+}
+
+export const POKEDEX_ENTRIES: iPokedexEntry[] = [
+  {
+    id: 359,
+    name: 'Absol',
+  },
+  {
+    id: 385,
+    name: 'Jirachi',
+  },
+];
