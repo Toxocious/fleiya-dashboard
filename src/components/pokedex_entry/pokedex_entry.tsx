907d81da5cd524c6e62fd4f37f245dc0c63491a4
Jess <toxocious@gmail.com>
2022-07-11 16:26:09 -0700

    Implement foundation for the pokedex_entry component

---
+++ src/components/pokedex_entry/pokedex_entry.tsx
@@ -0,0 +1,27 @@
+import { useParams } from 'react-router-dom';
+
+import { POKEDEX_ENTRIES } from '~/constants/pokedex_entries';
+
+export const PokedexEntry = () => {
+  const ROUTE_PARAMS = useParams();
+  const SPECIES_ID: any = ROUTE_PARAMS.id;
+
+  const SPECIES_DATA: any = POKEDEX_ENTRIES.filter(
+    (entry) => entry.id == SPECIES_ID
+  )?.[0];
+
+  if (typeof SPECIES_DATA === 'undefined') {
+    return (
+      <div className='page'>
+        <h2>ERROR</h2>
+        There is no information on this species of Pok&eacute;mon.
+      </div>
+    );
+  }
+
+  return (
+    <div className='page'>
+      <h2>{SPECIES_DATA.name}</h2>
+    </div>
+  );
+};
