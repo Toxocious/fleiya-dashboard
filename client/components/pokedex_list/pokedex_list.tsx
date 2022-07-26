d1e678a1aa607827280593b37a38e89b699f4b67
Jess <toxocious@gmail.com>
2022-07-25 17:02:18 -0700

    Implement Pokedex_List component and live filtering for Pokedex entries

---
+++ client/components/pokedex_list/pokedex_list.tsx
@@ -0,0 +1,44 @@
+import { PokedexCard } from '@components/pokedex_card';
+
+export const PokedexList = (props: any) => {
+  const { PokedexEntries, Filter } = props;
+
+  const FILTERED_LIST = PokedexEntries.filter((ENTRY: any) => {
+    switch (Filter) {
+      case '':
+        return ENTRY;
+
+      case 'Gen 1':
+        return ENTRY.Pokedex_ID <= 151;
+
+      case 'Gen 2':
+        return ENTRY.Pokedex_ID >= 152 && ENTRY.Pokedex_ID <= 251;
+
+      case 'Gen 3':
+        return ENTRY.Pokedex_ID >= 252 && ENTRY.Pokedex_ID <= 386;
+
+      case 'Gen 4':
+        return ENTRY.Pokedex_ID >= 387 && ENTRY.Pokedex_ID <= 493;
+
+      case 'Gen 5':
+        return ENTRY.Pokedex_ID >= 494 && ENTRY.Pokedex_ID <= 649;
+
+      case 'Gen 6':
+        return ENTRY.Pokedex_ID >= 650 && ENTRY.Pokedex_ID <= 721;
+
+      case 'Gen 7':
+        return ENTRY.Pokedex_ID >= 722 && ENTRY.Pokedex_ID <= 809;
+
+      default:
+        return ENTRY.Pokemon.toLowerCase().includes(Filter);
+    }
+  });
+
+  return (
+    <div className='flex-row'>
+      {FILTERED_LIST.map((entry: any) => (
+        <PokedexCard key={entry.ID} {...entry} />
+      ))}
+    </div>
+  );
+};
--- client/routes/pokedex/pokedex.tsx
+++ client/routes/pokedex/pokedex.tsx
@@ -1,12 +1,13 @@
-import { useEffect } from 'react';
+import { useEffect, useState } from 'react';
 import { useDispatch, useSelector } from 'react-redux';
 
 import { LoadingSvg } from '@components/loading_svg';
-import { PokedexCard } from '@components/pokedex_card';
+import { PokedexList } from '@components/pokedex_list';
 
 import { GET_POKEDEX_ENTRIES } from '@actions/pokedex_entry';
 
 export const PokedexPage = (props: any) => {
+  const [filter, setFilter] = useState('Gen 1');
   const dispatch = useDispatch();
 
   useEffect(() => {
@@ -14,19 +15,61 @@ export const PokedexPage = (props: any) => {
     dispatch(GET_POKEDEX_ENTRIES());
   }, [dispatch]);
 
+  const inputHandler = (e: any) => {
+    setFilter(e.target.value.toLowerCase());
+    console.log('filter:', filter);
+  };
+
   const ENTRIES: any = useSelector((state) => state);
 
   return (
     <main>
-      <h2>Pokedex</h2>
+      <div className='page-header'>
+        <h1>Pokedex</h1>
+
+        <aside>
+          <div>
+            <b>Generation</b>
+            <a href='#' onClick={() => setFilter('Gen 1')}>
+              I
+            </a>
+            <a href='#' onClick={() => setFilter('Gen 2')}>
+              II
+            </a>
+            <a href='#' onClick={() => setFilter('Gen 3')}>
+              III
+            </a>
+            <a href='#' onClick={() => setFilter('Gen 4')}>
+              IV
+            </a>
+            <a href='#' onClick={() => setFilter('Gen 5')}>
+              V
+            </a>
+            <a href='#' onClick={() => setFilter('Gen 6')}>
+              VI
+            </a>
+            <a href='#' onClick={() => setFilter('Gen 7')}>
+              VII
+            </a>
+          </div>
+          <div>
+            <input
+              type='text'
+              placeholder='Search For A Pok&eacute;mon'
+              onChange={inputHandler}
+            />
+          </div>
+        </aside>
+      </div>
 
       <div className='flex-row'>
         {typeof ENTRIES?.POKEDEX_ENTRIES?.payload == 'undefined' ? (
           <LoadingSvg />
         ) : (
-          ENTRIES.POKEDEX_ENTRIES.payload.map((entry: any) => (
-            <PokedexCard key={entry.ID} {...entry} />
-          ))
+          <PokedexList
+            PokedexEntries={ENTRIES.POKEDEX_ENTRIES.payload}
+            Filter={filter}
+          />
         )}
       </div>
     </main>
