c9d059b75c30268d9f1f5a3498b8b7953587c119
Jess <toxocious@gmail.com>
2022-07-28 19:35:11 -0700

    Implement featured_news home page feature

---
+++ client/features/featured_news/featured_news.tsx
@@ -0,0 +1,20 @@
+import './featured_news.scss';
+
+export const FeaturedNews = () => {
+  return (
+    <section className='featured_news christmas'>
+      <div className='feature_title'>
+        <h2>Christmas Event</h2>
+        <h3>July 2nd, 2022 &mdash; July 31st, 2022</h3>
+      </div>
+
+      <div className='feature_description'>
+        <h3>
+          The Christmas event is open once again! Come to Mt. Silver after
+          clearing the Kanto or Johto Elite 4 and join in the festivities and
+          hunt Pok&eacute;mon to your heart's content.
+        </h3>
+      </div>
+    </section>
+  );
+};
