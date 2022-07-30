16fb4268b6de85a6581f8172b9186c06a2dc48ed
Jess <toxocious@gmail.com>
2022-07-29 21:23:40 -0700

    Add dropdown template to current user

--- client/components/user_bar/user_data/user_data.tsx
+++ client/components/user_bar/user_data/user_data.tsx
@@ -1,4 +1,7 @@
+import { useState } from 'react';
 import { ChevronDown } from 'tabler-icons-react';
+
+import { Dropdown } from '@components/dropdown';
 
 import './user_data.scss';
 
@@ -7,15 +10,35 @@ const mock_user = {
   user_name: 'Toxocious',
   membership: 'Premium',
   avatar: '/avatar.jpeg',
+  links: [
+    {
+      id: 1,
+      label: 'Settings',
+      link: '/settings',
+    },
+    {
+      id: 2,
+      label: 'Log Out',
+      link: '/logout',
+    },
+  ],
 };
 
 export const UserData = () => {
+  const [showDropdown, setShowDropdown] = useState(false);
+
   return (
-    <section className='user_data'>
+    <section
+      className='user_data'
+      onMouseEnter={() => setShowDropdown(true)}
+      onMouseLeave={() => setShowDropdown(false)}
+    >
       <div>
         <img src={mock_user.avatar} />
         <ChevronDown size={14} />
       </div>
+
+      <Dropdown links={mock_user.links} hidden={!showDropdown} />
     </section>
   );
 };
