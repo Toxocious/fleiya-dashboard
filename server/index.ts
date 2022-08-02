7661a2a81fd4139cbac5391b93e866bb6a070a6f
Jess <toxocious@gmail.com>
2022-08-02 13:55:55 -0700

    Fix server env var checking

--- server/index.ts
+++ server/index.ts
@@ -1,16 +1,4 @@
-import * as path from 'path';
-import * as dotenv from 'dotenv';
-dotenv.config({ path: path.resolve(__dirname, './.env') });
-
-if (typeof process.env.ATLAS_URI === 'undefined') {
-  console.log(`[! | Error] ATLAS_URI env var is missing`);
-  process.exit(1);
-}
-
-if (typeof process.env.MONGODB_PORT === 'undefined') {
-  console.log(`[! | Error] MONGODB_PORT env var is missing`);
-  process.exit(1);
-}
+import { ATLAS_URI, MONGODB_PORT } from './auth/env_check';
 
 import express from 'express';
 import mongoose from 'mongoose';
@@ -28,12 +16,10 @@ app.use(cors());
 app.use('/pokedex', pokedexRoutes);
 
 mongoose
-  .connect(process.env.ATLAS_URI)
+  .connect(ATLAS_URI)
   .then(() =>
-    app.listen(process.env.MONGODB_PORT, () =>
-      console.log(
-        `[~ | Server] Running On: http://localhost:${process.env.MONGODB_PORT}`
-      )
+    app.listen(MONGODB_PORT, () =>
+      console.log(`[~ | Server] Running On: http://localhost:${MONGODB_PORT}`)
     )
   )
   .catch((error) => console.log(`[! | Server] Unable to connect`, error));
