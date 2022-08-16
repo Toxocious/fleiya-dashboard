0fd964fe8968b606dc132beacd29cd8cb7ebe9ee
Jess <toxocious@gmail.com>
2022-08-16 15:19:17 -0700

    Import env_check into main server file

--- server/index.ts
+++ server/index.ts
@@ -1,3 +1,5 @@
+import './auth/env_check';
+
 import * as trpcExpress from '@trpc/server/adapters/express';
 import { appRouter } from './routers/index';
 import { createContext } from './routers/context';
@@ -9,7 +11,13 @@ const app = express();
 
 app.use(express.json({ limit: '30mb' }));
 app.use(express.urlencoded({ limit: '30mb', extended: true }));
-app.use(cors());
+
+app.use(
+  cors({
+    credentials: true,
+    origin: ['http://localhost:3000'],
+  })
+);
 
 // Use TRPC middleware
 app.use(
